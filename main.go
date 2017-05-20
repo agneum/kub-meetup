a7ccbbe287192b7229c78759d59067dd41f22359
Artem Kartasov <art.kartasov@gmail.com>
2017-05-20 15:37:38 +0700

    Middleware

--- main.go
+++ main.go
@@ -3,13 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/takama/router"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8888", nil)
+	r := router.New()
+
+	a := 5
+	// r.GET("/", home)
+	// r.POST("/pampam", pampam)
+	r.POST("/api/v1/users", mw(a, abc))
+
+	r.Listen(":8888")
+	// http.HandleFunc("/", handler)
+	// http.ListenAndServe(":8888", nil)
+}
+
+func home(c *router.Control) {
+	fmt.Fprintf(c.Writer, "URL.Path = %q\n", c.Request.URL.Path)
+}
+
+func mw(a int, h router.Handle) router.Handle {
+	return h
+}
+
+func abc(c *router.Control) {
+	fmt.Fprintf(c.Writer, "a = ?")
+}
+
+func pampam(c *router.Control) {
+	fmt.Fprintf(c.Writer, "URL.Path = %q\n", c.Request.URL.Path)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path=%q\n", r.URL.Path)
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	greeting, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedGreeting := "URL.Path = \"/\""
+	testingGreeting := strings.Trim(string(greeting), "\n")
+	if testingGreeting != expectedGreeting {
+		t.Fatalf("Wrong greeting '%s', expected '%s'", testingGreeting, expectedGreeting)
+	}
+}
